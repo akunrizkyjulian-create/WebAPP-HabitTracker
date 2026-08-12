1fb3d3aed77b01324fbb4ea3bbd621910e090c51
Rizky julian <akunrizkyjulian17@gmail.com>
2026-08-12 11:58:39 +0800

    Tampilkan data api menggunakan javascript fetch

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,28 @@
-console.log('app.js berhasil dimuat');
+const API_URL = 'http://127.0.0.1:8000/api';
+
+async function ambilHabits() {
+    const response = await fetch (`${API_URL}/habits`);
+    const habits = await response.json();
+    tampilkanHabits(habits);
+}
+
+function tampilkanHabits(habits) {
+    const container = document.getElementById('app');
+
+    if (habits.length === 0) {
+        container.innerHTML = '<p>Belum ada Habit.</p>';
+        return;
+    }
+
+    let html = '';
+
+    habits.forEach(habit => {
+        html += `<div class="habit-card"> 
+            <h3>${habit.name}</h3>
+        </div>`;
+    });
+
+    container.innerHTML = html;
+}
+
+ambilHabits()
